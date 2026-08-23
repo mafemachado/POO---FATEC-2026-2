89a73cd4956c8960023a04840eaea6c76af4fd1b
Maria Fernanda Machado <mafesouxa@gmail.com>
2026-08-23 19:45:52 -0300

    Add files via upload

---
+++ Exemplo4/src/Exemplo4.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author maria
+ */
+public class Exemplo4 {
+    public static void main(String[] args) {
+        int cont, num;
+        
+        num = (int) (Math.random() * 100);
+        System.out.println("\t\t\t TABUADA DO " + num);
+        for (cont = 1; cont <= 10; cont++){
+            System.out.println(num + " x " + cont + " = " + cont * num);
+        }
+    }
+    
+}
