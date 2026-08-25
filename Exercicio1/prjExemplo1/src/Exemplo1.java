c7585a7aa7ef8b9bc45e04b6395487f26c692642
Maria Fernanda Machado <mafesouxa@gmail.com>
2026-08-25 09:52:07 -0300

    Add files via upload

---
+++ Exercicio1/prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author maria
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        System.out.println("Programação Orientada a Objetos");
+        System.out.print("Linguagem Java");
+    }    
+}
