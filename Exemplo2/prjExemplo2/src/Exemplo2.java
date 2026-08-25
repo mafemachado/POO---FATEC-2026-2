6e5a3bb209fe8f1832a32dacce156dfaf92b5bb4
Maria Fernanda Machado <mafesouxa@gmail.com>
2026-08-25 09:55:03 -0300

    Add files via upload

---
+++ Exemplo2/prjExemplo2/src/Exemplo2.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author maria
+ */
+public class Exemplo2 {
+    public static void main(String[] args) {
+     int idade;
+     double altura;
+     char sexo;     
+     String nome;
+     int x, y, soma;
+	 
+     idade = 23;
+     altura = 1.75;
+     sexo = 'M';
+     nome = "Carlos Silveira";
+	 
+     System.out.println("Nome  : " + nome);
+     System.out.println("Sexo  : " + sexo);
+     System.out.println("Idade : " + idade);
+     System.out.println("Altura: " + altura);  
+     
+     x=10;
+     y=15;
+     
+     System.out.println("Soma de " + x + " com " + y + " é " +  (x + y));
+     
+    }    
+}
